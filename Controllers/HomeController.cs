3f85f76ba293c0ee0852602fca606939348e2f11
Alamin-Bhuiyan <amialaminb@gmail.com>
2024-08-19 08:30:46 +0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController() 
+        {
+            _employeeRepository = new MockEmployeeRepository();
+        }
+        [Route("")]
+        [Route("/")]
+        [Route("Index")]
+        public ViewResult Index() 
+        {
+            //var name = _employeeRepository.GetEmployee(1).Name;
+            var model = _employeeRepository.GetAllEmployee();
+            return View(model);
+        }
+        [Route("[action]/{id?}")]
+        public ViewResult Details(int? id)
+        {
+            HomeDetailsViewModel homeDetailsviewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(id ?? 1),
+                PageTitle = "Employee Details"
+            };
+            return View(homeDetailsviewModel);
+        }
+    }
+}
---
+++ Models/MockEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Alamin", Department = "CSE", Email = "alamin@gmail.com" },
+                new Employee() { Id = 2, Name = "vanga", Department = "CSE", Email = "alamin@gmail.com" }
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.First(e => e.Id == Id);
+        }
+    }
+}
