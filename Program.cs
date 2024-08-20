a7db6ad59a360a181ffa766179b7a85a1b817075
Alamin <amialaminb@gmail.com>
2024-08-20 19:12:53 +0600

    connection

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+builder.Services.AddDbContextPool<AppDbContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("EmployeeDBConnection")));
 
 var app = builder.Build();
 
